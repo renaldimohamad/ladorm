6d8385f5f141ec1fba61279c34ebb5ed3aac6b23
aldybala <renaldimohamad77@gmail.com>
2025-06-16 18:55:14 +0700

    feat(blog): add initial blog page layout

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,15 @@
+import { LayoutBlank } from "@/layouts";
+
+const Blog = () => {
+  return (
+    <LayoutBlank>
+      <div className="text-center">
+        <p className="text-[#203368]">
+          Hello Welcome To Blog Student Dormitory
+        </p>
+      </div>
+    </LayoutBlank>
+  );
+};
+
+export default Blog;
