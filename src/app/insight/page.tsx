38d4093449dbe0325b2027102590dd109ed7ef73
aldybala <renaldimohamad77@gmail.com>
2025-06-16 18:55:36 +0700

    feat(insight): add initial insight page layout

---
+++ src/app/insight/page.tsx
@@ -0,0 +1,15 @@
+import { LayoutBlank } from "@/layouts";
+
+const Insight = () => {
+  return (
+    <LayoutBlank>
+      <div className="text-center">
+        <p className="text-[#203368]">
+          Hello Welcome To Insight Student Dormitory
+        </p>
+      </div>
+    </LayoutBlank>
+  );
+};
+
+export default Insight;
