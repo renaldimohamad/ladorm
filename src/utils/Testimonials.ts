3b3b389b52c3fe0d7baee74265dcae4cfe62949e
aldybala <renaldimohamad77@gmail.com>
2025-06-29 12:32:49 +0700

    🐞 fix: testimonials and scroll direction animation section

---
+++ src/utils/Testimonials.ts
@@ -0,0 +1,56 @@
+export const testimonialsData = [
+  {
+    name: "Dea Rahmatia Sofyan",
+    avatar: "/images/Dea Sofyan.png",
+    message:
+      "Asrama tidak sesederhana tempat tinggal tapi asrama adalah rumah untuk pulang di tanah rantau. Terimakasih sudah berbagi cerita seatap dan serantau.",
+  },
+  {
+    name: "Nadia Nur Arifah Hulawa",
+    avatar: "/images/Nadia Arifah.png",
+    message:
+      "Hidup di asrama? Tiap saat ada episode baru. Banyakkk tipe dan warna manusia yang bikin banyak belajarnya. ISTIMEWA, mau disini seterusnya.",
+  },
+  {
+    name: "Frastika Damopolii",
+    avatar: "/images/Frastika Damopolii.png",
+    message:
+      "For me, dormitory is a second home and the people in it are family.",
+  },
+  {
+    name: "Rahmawati Musa",
+    avatar: "/images/rati-musa.png",
+    message:
+      "Sebagai cewek, aku sempat worry soal kenyamanan dan keamanan. Tapi setelah tinggal di LADORM, semua keraguan itu hilang. Fasilitasnya cukup oke, lingkungannya aman, dan yang paling penting orang-orangnya saling jaga satu sama lain. Di sini aku belajar banyak hal di luar teori kuliah.",
+  },
+  {
+    name: "Yoanli Mananohas",
+    avatar: "/images/yoanli-image.png",
+    message:
+      "Tinggal di Asrama Gorontalo Lenteng Agung bikin saya ngerasa kayak punya keluarga kedua di Jakarta. Di sini bukan cuma tempat tinggal, tapi juga tempat bertumbuh dan saling support antar sesama anak rantau.",
+  },
+  {
+    name: "Alfian Naway",
+    avatar: "/images/Fian Naway.png",
+    message:
+      "Asrama menjadi pilihan terbaik ketika rancangan ruang tak sekadar memayungi tubuh, tetapi juga merangkul jiwa menghadirkan harmoni antara fungsi, kenyamanan, dan rasa memiliki.",
+  },
+  {
+    name: "Taufiq Hidayatullah Balu",
+    avatar: "/images/Taufiq hidyatullah Balu.png",
+    message:
+      "Asrama menjadi pilihan terbaik ketika rancangan ruang tak sekadar memayungi tubuh, tetapi juga merangkul jiwa menghadirkan harmoni antara fungsi, kenyamanan, dan rasa memiliki.",
+  },
+  {
+    name: "Dicky Prasandi Potutu",
+    avatar: "/images/Dky Potutu.png",
+    message:
+      "Awalnya, saya kira tinggal di asrama itu bakal kaku dan ribet, tapi ternyata beda banget. LADORM sangat ramah dan kekeluargaannya kerasa. Tiap malam sering ngumpul bareng buat ngobrol atau ngerjain tugas bareng. Kalau capek kuliah, balik ke sini rasanya kayak pulang ke rumah.",
+  },
+  {
+    name: "Fian Hamzah",
+    avatar: "/images/Alfian K. Hamzah.png",
+    message:
+      "LADORM itu tempat paling pas buat mahasiswa yang ingin belajar mandiri, tapi tetap punya support system. Dari kegiatan bareng sampe diskusi ringan di teras asrama, semua pengalaman di sini bikin gue lebih dewasa dan tangguh.",
+  },
+];
