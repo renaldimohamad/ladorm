50571e95e9a2ac8391d542fc5c7c4378168bbe50
aldybala <renaldimohamad77@gmail.com>
2026-04-25 15:21:05 +0700

    chore: optimize prisma initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,19 @@
 import { PrismaClient } from '../generated/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  })
 }
 
 declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-const prisma = prismaClientSingleton()
-
-export default prisma
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = prisma
 }
+
+export default prisma
